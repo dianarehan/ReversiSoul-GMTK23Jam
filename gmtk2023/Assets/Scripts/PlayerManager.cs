3b06088707b5f618e3cf6ec11a335bf2221dec57
nanufriev <nikita.anufriev@mytona.com>
2023-07-09 14:00:37 +0300

    Fix some logic

--- gmtk2023/Assets/Scripts/PlayerManager.cs
+++ gmtk2023/Assets/Scripts/PlayerManager.cs
@@ -7,18 +7,32 @@ public class PlayerManager : MonoBehaviour
     public CinemachineVirtualCamera virtualCamera;
     public float BodySwapCooldown = 2;
     private bool canBodySwap = true;
+    public static PlayerManager instance;
 
     public CharacterController startCharacter;
-    private CharacterController currentCharacter;
+    public CharacterController currentCharacter;
 
     private WaitForSeconds bodySwapCooldownDelay;
     private Coroutine bodySwapCooldownCoroutine;
-    private bool spaceInput = false;
-    private bool mouseInput = false;
+    private bool spaceInput;
+    private bool mouseInput;
+    private float xAxis;
+    private float yAxis;
     
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        
         currentCharacter = startCharacter;
+        currentCharacter.Speed *= 1.5f;
+        currentCharacter.GetComponent<Rigidbody2D>().mass = 10;
     }
 
     private void Start()
@@ -54,8 +68,16 @@ public class PlayerManager : MonoBehaviour
 
     private void TryChangeTarget(CharacterController newTarget)
     {
+        currentCharacter.Speed /= 1.5f;
+        currentCharacter.GetComponent<Rigidbody2D>().mass = 1;
+
+
         currentCharacter = newTarget;
         
+        currentCharacter.Speed *= 1.5f;
+        currentCharacter.GetComponent<Rigidbody2D>().mass = 10;
+
+
         if (bodySwapCooldownCoroutine != null)
         {
             StopCoroutine(BodySwap());
@@ -69,12 +91,15 @@ public class PlayerManager : MonoBehaviour
 
     private void Update()
     {
-        spaceInput = Input.GetKeyDown(KeyCode.Space);
-        mouseInput = Input.GetKeyDown(KeyCode.Mouse0);
-        
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+    }
+    
+    private void FixedUpdate()
+    {
         if (currentCharacter != null)
         {
-            currentCharacter.Move(spaceInput, mouseInput);
+            currentCharacter.Move(xAxis, yAxis);
         }
     }
 
